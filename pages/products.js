1cda6c809765b75e77f7cdd48637a4fdbcd8f647
matylda-bit <58633008+matylda-bit@users.noreply.github.com>
2021-01-23 20:51:16 +0000

    add products page

---
+++ pages/products.js
@@ -0,0 +1,15 @@
+const Products = () => {
+  return (
+    <>
+      <h1>Winter hats</h1>
+      <p>Super warm and cool hats</p>
+      <p>£20</p>
+      <hr />
+      <h1>Winter gloves</h1>
+      <p>Wool blend and leather gloves</p>
+      <p>£17</p>
+    </>
+  )
+}
+
+export default Products
